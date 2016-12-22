e8c68cd763edb0651375c6c11e540db13ec0b3b5
yamatokoakito <abletse@yahoo.co.jp>
2016-12-22 13:08:11 +0900

    no message

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ページ内スクロール
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
